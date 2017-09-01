cdbc81ab9251e6db0674680abd101882d463e7d7
Marat <vanilla-thunder@users.noreply.github.com>
2017-09-01 10:38:26 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,23 @@
-chrome.bookmarks.onCreated.addListener(function (id, bookmark) {
-    chrome.storage.sync.get('selected', function(data) {
+var watch = false;
+chrome.bookmarks.onCreated.addListener(function(id, bookmark) 
+{
+    if(!this.watch) return;
+    console.log("new bookmark");
+    console.log(bookmark);
+    chrome.storage.sync.get('selected', function(data) 
+    {
         if(bookmark.parentId !== data.selected) return;
         chrome.tabs.create({ url: bookmark.url });
         chrome.bookmarks.remove(id,function() { });
     });
+});
+chrome.bookmarks.onImportBegan.addListener(function () 
+{
+    console.log("import began");
+    this.watch = true;
+});
+chrome.bookmarks.onImportEnded.addListener(function () 
+{
+    console.log("import ended");
+    this.watch = false;
 });
